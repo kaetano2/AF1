90f7099d47aa535ad9188c615ec8604d7689332f
Kaetano Rodrigues <kaetanokako23@gmail.com>
2021-11-19 19:58:44 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <Sbar/>
+    <Appbar/>
+    <Navbar/>
+    <v-main>
+      
+    </v-main>
+    <Footer/>
+    <Vbotton/>
+  </v-app>
+</template>
+
+<script>
+
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Vbotton from './components/Vbotton';
+import Sbar from './components/Sbar';
+import Appbar from './components/Appbar';
+
+export default {
+  name: 'App',
+
+  components: {
+    Footer,
+    Appbar,
+    Navbar,
+    Vbotton,
+    Sbar,
+    
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
